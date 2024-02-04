5c2f49d0ff8a948e33abc6f5123f252342bb8d74
ruby <music24241@gmail.com>
2024-02-04 20:35:11 +0800

    feat: init project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <a href="#" class="btn btn-primary">test</a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style lang="scss">
+@import "bootstrap/scss/bootstrap";
+</style>
